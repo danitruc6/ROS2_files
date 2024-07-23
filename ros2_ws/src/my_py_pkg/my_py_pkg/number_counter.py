de12327bd6e3d194dc0e3bda055c0e7839a2eea1
danitruc6 <danitruc6@hotmail.com>
2024-07-23 16:43:27 -0600

    adding nodes and zsh config file

---
+++ ros2_ws/src/my_py_pkg/my_py_pkg/number_counter.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from example_interfaces.msg import Int64
+
+
+class NumberCounterNode(Node):  # MODIFY NAME
+    def __init__(self):
+        super().__init__("number_counter")  # MODIFY NAME
+        self.number_ = 0
+        self.counter_ = 0
+        self.subscriber_ = self.create_subscription(
+            Int64, "number", self.callback_number, 10
+        )
+        self.publisher_ = self.create_publisher(Int64, "number_count", 10)
+        self.timer_ = self.create_timer(0.5, self.publish_number_count)
+        self.get_logger().info("Number counter has been started")
+
+    def callback_number(self, msg):
+        self.counter_ += msg.data
+        self.get_logger().info(str(msg.data))
+
+    def publish_number_count(self):
+        msg = Int64()
+        msg.data = self.counter_
+        self.publisher_.publish(msg)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = NumberCounterNode()  # MODIFY NAME
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
