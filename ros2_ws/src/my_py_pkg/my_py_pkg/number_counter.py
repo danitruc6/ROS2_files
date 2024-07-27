b736edb5571f09063d7853afb9d4853997da4ee3
danitruc6 <danitruc6@hotmail.com>
2024-07-27 11:22:34 -0600

    adding new package

--- ros2_ws/src/my_py_pkg/my_py_pkg/number_counter.py
+++ ros2_ws/src/my_py_pkg/my_py_pkg/number_counter.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from example_interfaces.srv import SetBool
 import rclpy
 from rclpy.node import Node
 from example_interfaces.msg import Int64
@@ -15,6 +16,21 @@ class NumberCounterNode(Node):  # MODIFY NAME
         self.publisher_ = self.create_publisher(Int64, "number_count", 10)
         self.timer_ = self.create_timer(0.5, self.publish_number_count)
         self.get_logger().info("Number counter has been started")
+
+        # creating service server
+        self.server_ = self.create_service(
+            SetBool, "reset_counter", self.callback_reset_counter
+        )
+
+    def callback_reset_counter(self, request, response):
+        if request.data:
+            self.counter_ = 0
+            response.success = True
+        else:
+            response.success = False
+            response.message = "The counter has not been reset"
+        self.get_logger().info(f"The input data is {request.data} and the response is {response.success}")
+        return response
 
     def callback_number(self, msg):
         self.counter_ += msg.data
